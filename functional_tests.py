2489c849703350ce2173563722e9b85f39f2a4cc
rcafalchio <ricardo.cafalchio@gmail.com>
2019-07-05 13:03:07 -0300

    Primeiro teste end-to-end - Validação de título

--- functional_tests.py
+++ functional_tests.py
@@ -1,11 +1,44 @@
+print("Inicio do teste ")
+
 from selenium import webdriver
+import unittest
 
-print("IMPORTOU SELENIUM WEBDRIVER")
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        
+        self.browser = webdriver.Firefox()
+    
+    def tearDown(self):
+        
+        self.browser.quit()
 
-browser = webdriver.Firefox()
+    def test_can_start_a_list_and_retrive_it_later(self):
 
-browser.get("http://localhost:8000")
+        #Edith ouviu falar de uma nova aplicação online interessante para lista de tarefas. 
+        #Ela decide verificar sua homepage
+        self.browser.get("http://localhost:8000")
 
-assert 'Django' in browser.title
+        #Ela percebe que o titulo da página e o cabeçalho mencionam lista de tarefas (to-do)
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Test Fail!')
 
-browser.close()
+        #Ela digita "Buy peacock feathers"(Comprar penas de pavão) em uma caixa de texto
+
+        #Quando ela tecla enter, a página é atualizada, e agora a página lista
+        # "1: Buy peacock feathers" como um item em uma lista de tarefas
+
+        #Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro item.
+        #Ela insere "Use peacock feathers to make a fly"
+
+        #A página é atulizada novamente e agora mostra dois itens em sua lista
+
+        #Edith se pergunta se o sita lembrará da lista. Então ela nota que o site gerou 
+        # uma URL único para ela -- há um pequeno texto explicativo para isso.
+
+        #Ela acessa esse URL - sua lista de tarefas continua lá.
+
+        #Satisfeita, ela volta a dormir.
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
